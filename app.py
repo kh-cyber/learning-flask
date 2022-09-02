23688720a745f1bf8c8e64915becb0c892ef03f1
kh-cyber <khushbujindal2002@gmail.com>
2022-09-02 11:30:42 +0530

    about page

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ def index():
     return render_template('index.html')
     #return 'Hello, World!'
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
 # @app.route('/products')
 # def products():
 #     return 'this is product page'
